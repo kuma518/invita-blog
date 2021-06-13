bc1c13303c8270e695492ae99e301ed86079cdb6
junaga <hermann-stanew@invita.gmbh>
2021-06-13 15:02:56 +0200

    node working

---
+++ hello-world.js
@@ -0,0 +1,1 @@
+console.log("I am a javascript process. This is the system/environment I run in:", process.env)
---
+++ hello-world.sh
@@ -0,0 +1,6 @@
+echo "I am a Shell process. This is the system/environment I run in:"
+env
+
+echo -e "\n"
+bash --version
+node --version
