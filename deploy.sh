4d29d7babfd6ee6cc30826985b3423fa8a591460
junaga <hermann-stanew@invita.gmbh>
2021-06-17 20:15:05 +0200

    Finalize deploys to Firebase Hosting (#4)

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# WARNING: run from project root only.
+# Make a manual deploy from the local dev environment.
+
+# We deploy to the serverless "Firebase" cloud platform "Hosting" CDN service.
+# About Firebase: https://firebase.google.com/products-build
+# About Firebase Hosting: https://firebase.google.com/products/hosting
+
+
+defaultProjectID='invita-blog'
+defaultChannelID='testing'
+# note: `live` is the name of the production Firebase Hosting site channel
+
+echo "== Deploying build using the Firebase CLI =="
+
+
+echo -e "\nIs everything cool?"
+# node_modules/ with `firebase` bin is installed.
+# the machine is signed in.
+# a dist/ got build.
+npx firebase login --no-localhost
+npx firebase projects:list
+
+
+echo -e "\nEnter your targets"
+# Prompt for conformation on all targets.
+read -p "[Required] Project ID: " -ei "$defaultProjectID" projectID
+read -p "[Required] Channel ID: " -ei "$defaultChannelID" channelID
+
+
+echo -e "\nRunning"
+npx firebase \
+  --project $projectID \
+  hosting:channel:deploy $channelID
